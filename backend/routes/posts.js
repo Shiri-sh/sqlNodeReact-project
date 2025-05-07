cc15651a3d0fe25c8b6a91ebebed2aa90ea883c4
Avigail Levy <avigail6282@gmail.com>
2025-05-07 20:40:41 +0300

    space

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -55,7 +55,7 @@ router.delete('/:id',async (req,res)=>{
     try {
         const id = req.params.id;
         await controller.deletePost(id);
-        res.status(200).json({ message: "Post deleted successfully!!!!!!!!!!!!!" });
+        res.status(200).json({ message: "Post deleted successfully!!!!!!!!!!!!" });
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Failed to delete post" });
